e356efa36f5a1761c53aab25e6eaeb181c22c701
ELKINANY <153640440+ELKINANY@users.noreply.github.com>
2025-08-19 20:04:05 +0300

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,6 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // 1) إنشاء transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST, 
     port: process.env.EMAIL_PORT,
